a9e8524840b3216c5669d0197236db30bc4946bd
gitusp <ususso@gmail.com>
2019-07-11 23:13:47 +0900

    Add test to prove an edge case of broadcasting
    It proves that disposing a sink while broadcasting breaks succeeding
    broadcasting. The reason it breaks succeeding broadcasting is that
    broadcasting is iterated with `forEach` method and `forEach` method does
    not care if the length of the target array is changed.

---
+++ src/index.test.ts
@@ -0,0 +1,22 @@
+import { tap, take, runEffects } from "@most/core";
+import { newDefaultScheduler } from "@most/scheduler";
+import { describe, it } from "mocha";
+import assert from "power-assert";
+import { createAdapter } from "./index";
+import { fake } from "sinon";
+
+describe("createAdapter", () => {
+  it("broadcasts to all sinks even when the sinks are removed while broadcasting", () => {
+    const [induce, events] = createAdapter();
+    const f1 = fake();
+    const f2 = fake();
+    const s1 = take(1, tap(f1, events));
+    const s2 = take(1, tap(f2, events));
+    const scheduler = newDefaultScheduler();
+    runEffects(s1, scheduler);
+    runEffects(s2, scheduler);
+    induce(undefined);
+    assert(f1.called);
+    assert(f2.called);
+  });
+});
