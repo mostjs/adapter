3f0496d384b85e2a9ea60e2fa01f7bf4ebb5e908
gitusp <ususso@gmail.com>
2019-07-25 22:36:58 +0900

    Revert lock implementation and add `.slice()`
    See https://github.com/mostjs/adapter/pull/3#issuecomment-514712173.

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,30 @@ export type Adapter<A, B> = [(event: A) => void, Stream<B>]
 
 export const createAdapter = <A> (): Adapter<A, A> => {
   const sinks: { sink: Sink<A>, scheduler: Scheduler }[] = []
-  let locked: boolean = false
-  const queue: { sink: Sink<A>, scheduler: Scheduler }[] = []
-  const removeSink = (sink: { sink: Sink<A>, scheduler: Scheduler }) => {
-    if (locked) {
-      queue.push(sink)
-    } else {
-      remove(sinks, sink)
-    }
-  }
-  return [a => {
-    locked = true
-    broadcast(sinks, a)
-    locked = false
-    if (queue.length > 0) {
-      queue.forEach(sink => remove(sinks, sink))
-      queue.length = 0
-    }
-  }, new FanoutPortStream(sinks, removeSink)]
+  return [a => broadcast(sinks, a), new FanoutPortStream(sinks)]
 }
 
 const broadcast = <A> (sinks: { sink: Sink<A>, scheduler: Scheduler }[], a: A): void =>
-  sinks.forEach(({ sink, scheduler }) => tryEvent(scheduler.currentTime(), a, sink))
+  sinks.slice().forEach(({ sink, scheduler }) => tryEvent(scheduler.currentTime(), a, sink))
 
 export class FanoutPortStream<A> {
-  constructor (
-    private readonly sinks: { sink: Sink<A>, scheduler: Scheduler }[],
-    private readonly removeSink: (sink: { sink: Sink<A>, scheduler: Scheduler }) => void
-  ) {}
+  constructor (private readonly sinks: { sink: Sink<A>, scheduler: Scheduler }[]) {}
 
   run (sink: Sink<A>, scheduler: Scheduler): Disposable {
     const s = { sink, scheduler }
     this.sinks.push(s)
-    return new RemovePortDisposable(s, this.removeSink)
+    return new RemovePortDisposable(s, this.sinks)
   }
 }
 
 export class RemovePortDisposable<A> implements Disposable {
-  constructor (
-    private readonly sink: { sink: Sink<A>, scheduler: Scheduler },
-    private readonly removeSink: (sink: { sink: Sink<A>, scheduler: Scheduler }) => void
-  ) {}
+  constructor (private readonly sink: { sink: Sink<A>, scheduler: Scheduler }, private readonly sinks: { sink: Sink<A>, scheduler: Scheduler }[]) {}
 
   dispose () {
-    this.removeSink(this.sink)
+    const i = this.sinks.indexOf(this.sink)
+    if(i >= 0) {
+      this.sinks.splice(i, 1)
+    }
   }
 }
 
@@ -56,12 +36,5 @@ function tryEvent <A> (t: Time, a: A, sink: Sink<A>) {
     sink.event(t, a)
   } catch(e) {
     sink.error(t, e)
-  }
-}
-
-function remove <A> (arr: A[], elm: A) {
-  const i = arr.indexOf(elm)
-  if (i >= 0) {
-    arr.splice(i, 1)
   }
 }
